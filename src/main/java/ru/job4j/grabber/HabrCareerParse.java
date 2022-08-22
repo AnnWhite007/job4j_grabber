c2eac01d71a8836a58d9359ff9e76627b327fef7
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-08-22 17:37:16 +0300

    2.4. HabrCareerParse [#285213 #326512] v.03

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -55,32 +55,17 @@ import java.util.List;
 
 public class HabrCareerParse implements Parse {
 
+    public static final int LIMIT = 5;
+
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     private final DateTimeParser dateTimeParser;
-    static final int LIMIT = 5;
+
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
-    }
-
-    public static void main(String[] args) throws IOException {
-        for (int p = 1; p <= LIMIT; p++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + p);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateElement = row.select(".vacancy-card__date").first();
-                String dateTime = dateElement.child(0).attr("datetime");
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
-            });
-        }
     }
 
     private String retrieveDescription(String link) throws IOException {
@@ -109,15 +94,18 @@ public class HabrCareerParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> vacancyList = new ArrayList<>();
-        final int LIMIT = 5;
-        for (int p = 1; p <= LIMIT; p++) {
-            Connection connection = Jsoup.connect(link + p);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> vacancyList.add(getPost(row)));
-        }
+            for (int p = 1; p <= LIMIT; p++) {
+                try {
+                    Connection connection = Jsoup.connect(link + p);
+                    Document document = connection.get();
+                    Elements rows = document.select(".vacancy-card__inner");
+                    rows.forEach(row -> vacancyList.add(getPost(row)));
+                } catch (IOException i) {
+                    throw new IllegalArgumentException();
+                }
+            }
         return vacancyList;
     }
 }
